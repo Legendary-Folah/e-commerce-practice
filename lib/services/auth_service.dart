915f93b07a2d529c6e1c6e0431f2624c47890c2d
FolaDev <folarinfadeyi@gmail.com>
2025-05-30 16:24:46 +0100

    feat: done logic for firebase

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -22,6 +22,32 @@ class AuthService {
         'password': password.trim(),
         'role': role,
       });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return e.toString();
+    }
+  }
+
+  Future<String?> logIn({
+    required String email,
+    required String password,
+    required String role,
+  }) async {
+    try {
+      final credential = await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+
+      DocumentSnapshot userDoc = await _firestore
+          .collection('users')
+          .doc(credential.user?.uid)
+          .get();
+      return userDoc['role'];
     } catch (e) {
       return e.toString();
     }
